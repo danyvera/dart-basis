3991d0009ad941f1c58631fb071e6e05c9271f85
Daniela Vera <54813226+danyvera@users.noreply.github.com>
2021-10-04 17:56:48 -0500

    Update Funciones.dart

--- Funciones.dart
+++ Funciones.dart
@@ -2,19 +2,23 @@ void main() {
   
   final nombre = 'Fernando';
   
-//   saludar( nombre, 'Greetings' );
+  saludar( nombre, 'Greetings' );
+  
   saludar2( nombre: nombre, mensaje: 'Greetings' );
   
 }
 
+                           //el argumento "mensaje" es argumento opcional: si no manda ningun valor, entonces el valor del argumento mensaje será "Hi"
 void saludar( String nombre, [ String mensaje = 'Hi' ]) {
   print('$mensaje $nombre');
 }
 
 
 void saludar2({ 
+  //para obligar a que el nombre sea siempre requerido: usamos la palabra "required"
   required String nombre, 
-  required String mensaje,
+  String mensaje = 'No message',
  }) {
   print('$mensaje $nombre');
+  print('Hola Mundo');
 }

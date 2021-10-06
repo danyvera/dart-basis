6a5aa32150fe0d086ca8ce25416759907faa0a13
Daniela Vera <54813226+danyvera@users.noreply.github.com>
2021-10-05 19:12:00 -0500

    Update Extends.dart

--- Extends.dart
+++ Extends.dart
@@ -1,20 +1,25 @@
+//la palabra reservada "extends" de una clase sirve para que podamos extender una clase con funcionalidades de otra y tambien podemos utilizar clases abstractas
+//cuando yo extiendo con "extends", eso quiere decir que en la clase en la que extiendo, ya puedo utilizar las propiedades, metodos y nombres de la clase extendida
+
 void main() {
     
   final superman = new Heroe('Clark Kent');
   final luthor   = new Villano('Lex Luthor');
  
-  
   print( superman );
+  print( superman.valentia );
   print( luthor );
+  print( superman.maldad );
 }
 
-
+//la clase abstracta
 abstract class Personaje {
   String? poder;
   String nombre;
   
-  Personaje( this.nombre );
+  Personaje( this.nombre ); //constructor de "Personaje"
   
+  //override sirve para sobreescribir
   @override
   String toString() {
     return '$nombre - $poder';
@@ -26,7 +31,9 @@ abstract class Personaje {
 class Heroe extends Personaje {
  
    int valentia = 100;
-  
+  // los dos puntos ":" significa que se va a ejecutar al momento de la inicializacion de mi clase
+                           //con "super", se manda a llamar al constructor de la clase en la cual se extiende "super"
+                            //en este caso, estamos apuntando al constructor de "Personaje"
    Heroe( String nombre ): super( nombre );
   
 }
